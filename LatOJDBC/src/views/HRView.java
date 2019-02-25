2d0bf69213b0d6cf73bebd79fbcc77a3b64ce9c6
Pandu Galang <pandu4431@gmail.com>
2019-02-25 22:04:37 +0700

    u2

--- LatOJDBC/src/views/HRView.java
+++ LatOJDBC/src/views/HRView.java
@@ -807,7 +807,7 @@ public class HRView extends javax.swing.JFrame {
 
     private void locationMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_locationMenuActionPerformed
         // TODO add your handling code here:
-        LocationView lv = new LocationView();
+        LocationViews lv = new LocationViews();
         jPanelMain.add(lv);
         lv.setVisible(true);
     }//GEN-LAST:event_locationMenuActionPerformed
