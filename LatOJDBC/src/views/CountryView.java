c43cc286c3059560be694cd930476883f19e308f
Gery Cahyandanu <gerydanu@live.com>
2019-02-26 10:17:19 +0700

    Merge branch 'master' of https://github.com/bootcamp23-mii/lembaranBaru
    # Conflicts:
    #	LatOJDBC/build/classes/views/RegionView.class

--- LatOJDBC/src/views/CountryView.java
+++ LatOJDBC/src/views/CountryView.java
@@ -387,6 +387,7 @@ public class CountryView extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jTableCountriesMouseClicked
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        jTextCountryId.setEnabled(true);
         jTextCountryId.setEditable(true);
         jTextCountryId.setText("");
         jTextCountryName.setText("");
